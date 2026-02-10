1be06575999e80a5fe36298d2c348c4472e3023a
basarab <joker-power@yandex.ru>
2026-02-10 21:12:14 +0300

    clean up

--- assets/scripts/game/mechanics/boosters/Booster.ts
+++ assets/scripts/game/mechanics/boosters/Booster.ts
@@ -2,7 +2,6 @@ import { GameEvent } from "../../../GameEvent";
 import { BoosterType } from "../../enums/BoosterType";
 import { InputState } from "../../enums/InputState";
 import { TurnClickProcessor } from "../../TurnProcessor";
-import { Board } from "../Board";
 
 export interface Booster extends TurnClickProcessor {
     readonly type: BoosterType;
